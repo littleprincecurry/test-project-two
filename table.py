a49476af3aa141596e676b2cdf5b71a4a91df261
littleprincecurry <scotwilson@gmail.com>
2020-12-23 17:21:04 -0500

    try again x2

--- table.py
+++ table.py
@@ -3,6 +3,6 @@ import pandas as pd
 
 
 def returnTable():
-    df = pd.read_json('/news')
+    df = pd.read_json('https://test-project-two.herokuapp.com/news')
     news_table = pd.to_html(df)
     return news_table
